c7e3ed64210152a462aed297d123c6add1a13add
Kim Bellander <kim.bellander@gmail.com>
2024-10-06 19:36:28 +0200

    Added method to present board, with the array inside.

--- src/Game.java
+++ src/Game.java
@@ -17,6 +17,7 @@ public class Game {
     public void setUp() {
         System.out.println("You are to play the game Tic Tac Toe.");
         System.out.println("This is what the playing board looks like:\n");
+        presentBoard();
         System.out.println("Player 1, please type your name:");
         String namePlayer1 = scanner.nextLine();
         System.out.println("Player 2, please type your name:");
@@ -24,5 +25,12 @@ public class Game {
         this.player1 = new Player(namePlayer1);
         this.player2 = new Player(namePlayer2);
         System.out.println(this.player1.getName() + ", your marker is an X. " + this.player2.getName() + ", your marker is an O.");
+    }
+
+    public void presentBoard() {
+        System.out.println("+----+----+----+\n| " +
+                positions.get(0)+"  | "+positions.get(1)+"  | "+positions.get(2)+"  |\n+----+----+----+\n| "+
+                positions.get(3)+"  | "+positions.get(4)+"  | "+positions.get(5)+"  |\n+----+----+----+\n| "+
+                positions.get(6)+"  | "+positions.get(7)+"  | "+positions.get(8)+"  |\n+----+----+----+\n");
     }
 }
