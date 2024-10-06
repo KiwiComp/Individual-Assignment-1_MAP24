52d0195200634ea5535cff9b7afc5c87b8aec1e6
Kim Bellander <kim.bellander@gmail.com>
2024-10-06 20:31:34 +0200

    Created method for placing markers, works to change into symbols on the board instead of numbers. Works to break loop when someone wins. TODO: try-catch if something other than an int is input, also if other ints are input. Furthermore, created method for checking if there is a winner. TODO: Check to see if there is a draw.

--- src/Game.java
+++ src/Game.java
@@ -9,18 +9,37 @@ public class Game {
     Scanner scanner = new Scanner(System.in);
 
     public Game() {
-        for(int i = 1; i<=9; i++) {
+        for (int i = 1; i <= 9; i++) {
             positions.add(i);
         }
     }
 
 
     public void playGame() {
+
         setUp();
-        placeMarker(player1, "X");
+
+        boolean thereIsAWinner = false;
+
+        while (!thereIsAWinner) {
+            placeMarker(player1, "X");
+
+            thereIsAWinner = checkWinner();
+            if(thereIsAWinner) {
+                break;
+            }
+
+            placeMarker(player2, "O");
+
+            thereIsAWinner = checkWinner();
+            if(thereIsAWinner) {
+                break;
+            }
+        }
+
     }
 
-//-----------------------------------METHOD: SET UP WITH PLAYER NAMES---------------------------------------------------
+    //-----------------------------------METHOD: SET UP WITH PLAYER NAMES---------------------------------------------------
     public void setUp() {
         System.out.println("You are to play the game Tic Tac Toe.");
         System.out.println("This is what the playing board looks like:\n");
@@ -34,31 +53,59 @@ public class Game {
         System.out.println("\n" + this.player1.getName() + ", your marker is an X. " + this.player2.getName() + ", your marker is an O.");
     }
 
-//------------------------METHOD: PRESENT THE CURRENT PLAYING BOARD-----------------------------------------------------
+    //------------------------METHOD: PRESENT THE CURRENT PLAYING BOARD-----------------------------------------------------
     public void presentBoard() {
         System.out.println("+----+----+----+\n| " +
-                positions.get(0)+"  | "+positions.get(1)+"  | "+positions.get(2)+"  |\n+----+----+----+\n| "+
-                positions.get(3)+"  | "+positions.get(4)+"  | "+positions.get(5)+"  |\n+----+----+----+\n| "+
-                positions.get(6)+"  | "+positions.get(7)+"  | "+positions.get(8)+"  |\n+----+----+----+\n");
+                positions.get(0) + "  | " + positions.get(1) + "  | " + positions.get(2) + "  |\n+----+----+----+\n| " +
+                positions.get(3) + "  | " + positions.get(4) + "  | " + positions.get(5) + "  |\n+----+----+----+\n| " +
+                positions.get(6) + "  | " + positions.get(7) + "  | " + positions.get(8) + "  |\n+----+----+----+\n");
     }
 
 
     public void placeMarker(Player player, String symbol) {
-        System.out.println(player + ", choose a position, 1-9, where you wish to place your marker.");
+        System.out.println(player.getName() + ", choose a position, 1-9, where you wish to place your marker.");
 
         boolean validInput = false;
-        while(!validInput) {
+        while (!validInput) {
             int chosenMove = scanner.nextInt();
             scanner.nextLine();
 
-            if(chosenMove >= 1 && chosenMove <=9) {
-                if(positions.get(chosenMove-1) instanceof String) {
+            if (chosenMove >= 1 && chosenMove <= 9) {
+                if (positions.get(chosenMove - 1) instanceof String) {
                     System.out.println("You cannot place your marker here. Enter a new position, please.");
                 } else {
-                    positions.set(chosenMove-1, symbol);
+                    positions.set(chosenMove - 1, symbol);
+                    presentBoard();
                     validInput = true;
                 }
             }
         }
+    }
+
+
+    private boolean checkWinner() {
+        if ((positions.get(0).equals("X") && positions.get(1).equals("X") && positions.get(2).equals("X")) ||
+                (positions.get(3).equals("X") && positions.get(4).equals("X") && positions.get(5).equals("X")) ||
+                (positions.get(6).equals("X") && positions.get(7).equals("X") && positions.get(8).equals("X")) ||
+                (positions.get(0).equals("X") && positions.get(3).equals("X") && positions.get(6).equals("X")) ||
+                (positions.get(1).equals("X") && positions.get(4).equals("X") && positions.get(7).equals("X")) ||
+                (positions.get(2).equals("X") && positions.get(5).equals("X") && positions.get(8).equals("X")) ||
+                (positions.get(0).equals("X") && positions.get(4).equals("X") && positions.get(8).equals("X")) ||
+                (positions.get(2).equals("X") && positions.get(4).equals("X") && positions.get(6).equals("X"))) {
+            System.out.println(player1.getName() + " is the winner! Congratulations!");
+            return true;
+        } else if ((positions.get(0).equals("O") && positions.get(1).equals("O") && positions.get(2).equals("O")) ||
+                (positions.get(3).equals("O") && positions.get(4).equals("O") && positions.get(5).equals("O")) ||
+                (positions.get(6).equals("O") && positions.get(7).equals("O") && positions.get(8).equals("O")) ||
+                (positions.get(0).equals("O") && positions.get(3).equals("O") && positions.get(6).equals("O")) ||
+                (positions.get(1).equals("O") && positions.get(4).equals("O") && positions.get(7).equals("O")) ||
+                (positions.get(2).equals("O") && positions.get(5).equals("O") && positions.get(8).equals("O")) ||
+                (positions.get(0).equals("O") && positions.get(4).equals("O") && positions.get(8).equals("O")) ||
+                (positions.get(2).equals("O") && positions.get(4).equals("O") && positions.get(6).equals("O"))) {
+            System.out.println(player2.getName() + " is the winner! Congratulations!");
+            return true;
+        }
+
+        return false;
     }
 }
